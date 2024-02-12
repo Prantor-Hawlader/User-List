42294a919ed74a534d10d821a49fb7fb12d7935f
Prantor-Hawlader <prantor502@gmail.comm>
2024-02-12 18:16:42 +0600

    Form heading added

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,17 @@ function App() {
     <div className="bg-slate-950 pb-10">
       <p
         className="py-10 text-center pb-2 text-3xl  font-bold text-green-500 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600
-          bg-[length:20%_6px] bg-no-repeat bg-bottom"
+          bg-[length:15%_6px] bg-no-repeat bg-bottom"
       >
         User List
       </p>
       <UserList />
+      <p
+        className="mt-16 text-center pb-2 text-3xl  font-bold text-green-500 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600
+          bg-[length:40%_6px] bg-no-repeat bg-bottom"
+      >
+        Add user to see the updated user card
+      </p>
       <UserForm />
     </div>
   );
